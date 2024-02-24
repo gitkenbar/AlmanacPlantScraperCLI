83708406198b611cb83b1cc44b3362a1290b395a
gitkenbar <137327896+gitkenbar@users.noreply.github.com>
2024-02-24 16:56:53 +0000

    Robust improvement to Nokogiri selectors

--- README.md
+++ README.md
@@ -6,16 +6,16 @@ This CLI scrapes data from Almanac.com and effortlessly displays it for the user
 Add technical documentation in the README.md file
 
 ### plant.rb
-Dynamically format paragraphs so that they render beautifully onto the page
+pests un-ordered list selector does not work properly
 
 ### auth.rb
 Add a reset password option
 
 ### scraper.rb
-Learn to use advanced Nokogiri functions and selectors to better format data pulled!
 
 Add more comprehensive error handling
   - predict edge case issues
     - "()" removal is the only one I can see
     - The plant list has entries with "()" in them, but parenthesis are not in the URL and should be removed
     - zucchini appears on vegetable list as Zucchini and Summer Squash, but the URL is just zucchini
+    - should try to make plural if input is missing "s" or "es"
--- lib/plant.rb
+++ lib/plant.rb
@@ -8,20 +8,58 @@ class Plant
     doc = Scraper.scrape_plant(i)
     #-Plant Data variables
     @plant_name = doc.css('.page-title').text.strip
+
+    @plant_text_body = doc.css(
+      '.field--name-field-body > p,
+      .field--name-field-body > h3,
+      .field--name-field-body > figure,
+      .field--name-field-body > li'
+    )
+
+    @wit_wisdom = doc.css('.field--name-field-wit-and-wisdom > .field__item > ul > li')
+
+    @planting_info = doc.css(
+      '.field--name-field-planting > .field__item > p,
+     .field--name-field-planting > .field__item > h4,
+     .field--name-field-planting > .field__item > ul > li'
+    )
+
+    @growing = doc.css(
+      '.field--name-field-care > .field__label,
+      .field--name-field-care > .field__item > ul > li'
+    )
+
+    @harvesting = doc.css(
+      '.field--name-field-harvest  .field__item > p,
+      .field--name-field-harvest  .field__item > h4,
+      .field--name-field-harvest  .field__item > ul > li'
+    )
+
+    @pests_diseases = doc.css('.field--name-field-pests > .field__label,
+      .field--name-field-pests > .field__item > p')
+    @pests_table_title = doc.css('.field--name-field-pests > .field__item > .plant-table > table > caption')
+
+    @pests_diseases_table_headers = doc.css('.field--name-field-pests > .field__item > .plant-table > table > thead > tr > th')
+    @pests_diseases_rows = doc.css('.field--name-field-pests > .field__item > .plant-table > table > tbody > tr')
+    @pests_diseases_list = doc.css('.field--name-field-pests > .field__item')
+    @pests_rows_titles = @pests_diseases_rows.css('th')
+
+    @recommended_varieties = doc.css(
+      '.field--name-field__recommended > .field__label,
+      .field--name-field__recommended  .field__item > p,
+      .field--name-field__recommended .field__item > ul > li'
+    )
+
+    # needs to pull <p> <h3> <figure> <li>
+
+    @plant_type = doc.css('.field--name-field-plant-type-term')
     @botanical_name = doc.css('.field--name-field-botanicalname').text.strip
-    @plant_type = doc.css('.field--name-field-plant-type-term').text.strip
     @plant_flower_color = doc.css('.field--name-field-flower-color-term').text.strip
-    @plant_sun = doc.css('.field--name-field-sun-exposure-term').text.strip
     @plant_special = doc.css('.field--name-field-special-features-term').text.strip
-    @plant_text_body = doc.css('.field--name-field-body').text.strip
-    @wit_wisdom = doc.css('.field--name-field-wit-and-wisdom').text.strip
-    @planting_info = doc.css('.field--name-field-planting').text.strip
+
+    @plant_sun = doc.css('.field--name-field-sun-exposure-term')
     @plant_ph = doc.css('.field--name-field-soil-ph-term').text.strip
-    @plant_hardiness = doc.css('.plant-info .block-field-blocknodeplantfield-hardiness-zone-term').text.strip
-    @growing = doc.css('.field--name-field-care').text.strip
-    @harvesting = doc.css('.field--name-field-harvest').text.strip
-    @recommended_varieties = doc.css('.field--name-field__recommended').text.strip
-    @pests_diseases = doc.css('.field--name-field-pests').text.strip
+    @plant_hardiness = doc.css('.block-field-blocknodeplantfield-hardiness-zone-term').text.strip
 
     choose_option('main')
   rescue StandardError
@@ -41,8 +79,14 @@ class Plant
     puts @botanical_name
     puts @plant_flower_color
     puts @plant_special
-    puts @plant_text_body
-    puts @wit_wisdom
+    @plant_text_body.each do |field|
+      puts "  #{field.text}"
+      puts ' '
+    end
+    @wit_wisdom.each do |field|
+      puts "  #{field.text}"
+      puts ' '
+    end
   end
 
   def self.planting
@@ -50,23 +94,67 @@ class Plant
     puts @plant_sun
     puts @plant_ph
     puts @plant_hardiness
-    puts @planting_info
+    @planting_info.each do |field|
+      puts "  #{field.text}"
+      puts ' '
+    end
   end
 
   def self.care
     system('clear')
-    puts @growing
-    puts @harvesting
+    @growing.each do |field|
+      puts "  #{field.text}"
+      puts ' '
+    end
+    @harvesting.each do |field|
+      puts "  #{field.text}"
+      puts ' '
+    end
   end
 
   def self.pests
+    # if pests is in the form of a ul, it does not render
     system('clear')
-    puts @pests_diseases
+    @pests_table_title.text
+    if @pests_diseases_rows
+      @pests_diseases_rows.each do |field|
+        pest_name = field.css('th')
+        pest_data = field.css('td')
+        puts '-------'
+        puts "  #{pest_name.text}   Type: #{pest_data[0].text}"
+        puts '-------'
+        puts " -#{@pests_diseases_table_headers[2].text} "
+        puts "    #{pest_data[1].text}"
+        puts " -#{@pests_diseases_table_headers[3].text} "
+        puts "    #{pest_data[2].text}"
+        puts ''
+      end
+    elsif @pests_diseases
+      @pests_diseases.each do |field|
+        puts "  #{field.text}"
+        puts ' '
+      end
+    elsif @pests_diseases_list
+      @pests_diseases_list.each do |field|
+        puts "  #{field.text}"
+        puts ' '
+      end
+    else
+      puts 'There was an Issue'
+    end
   end
 
   def self.varieties
     system('clear')
-    puts @recommended_varieties
+    @recommended_varieties.each do |field|
+      puts "  #{field.text}"
+      puts ' '
+    end
+  end
+
+  def self.formatter(i)
+    puts "   #{i.text}"
+    puts ' '
   end
 
   def self.clear
