bdbf754073840771de2f4d5f474448b7b937a706
gitkenbar <kennethbarker12@gmail.com>
2024-09-10 14:58:06 -0500

    fixed syntax, removed extra tests
    fixed describe syntax on L19 from `#{plant}` to plant.text.
    Angular syntax doesn't work in ruby, who knew?
    
    removed extra tests
    temporarily cut extra tests to speed up debugging until I can make sure the tests actually work

--- spec/plant.spec.rb
+++ spec/plant.spec.rb
@@ -8,28 +8,28 @@ end
 
 RSpec.describe Plant do
     # This will cycle through all the plants on from the list and evaluate whether their articles have sections
-    before do 
-    end
     
     # Makes the list of plants to be cycled through
+
     List.make_list 
-    plant_array = List.array_check('vegetables')
+    plant_array = List.array_check('foliage')
     plant_array.each do |plant|
         puts plant
         Menu.choose_option(`#{plant}`)
-        describe `#{plant}` do
-            it 'has Article-Main' do
-                expect(Plant.check_article('main')).to eq(true)
-            end
-            it 'has Article-Planting' do
-                expect(Plant.check_article('planting')).to eq(true)
-            end
-            it 'has Article-Care' do
-                expect(Plant.check_article('care')).to eq(true)
-            end
-            it 'has Article-Varieties' do
-                expect(Plant.check_article('varieties')).to eq(true)
-            end
+        describe plant.text do
+            #it 'has Article-Main' do
+            #    expect(Plant.check_article('main')).to eq(false)
+            #end
+
+            #it 'has Article-Planting' do
+            #    expect(Plant.check_article('planting')).to eq(true)
+            #end
+            #it 'has Article-Care' do
+            #    expect(Plant.check_article('care')).to eq(true)
+            #end
+            #it 'has Article-Varieties' do
+            #    expect(Plant.check_article('varieties')).to eq(true)
+            #end
             it 'has Article-Pests' do
                 expect(Plant.check_article('pests')).to eq(true)
             end
